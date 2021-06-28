a813fa74cd88be51dfc74477a5473caaa6072949
random-overflow <graziano.digrande@aitho.it>
2021-06-28 15:19:46 +0200

    fix(deploy): change branch name

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:random-overflow/forkify.git master:gh-pages
+git push -f git@github.com:random-overflow/forkify.git main:gh-pages
 
 cd -
